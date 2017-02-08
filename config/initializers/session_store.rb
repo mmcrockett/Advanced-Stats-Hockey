9f00979d9d22bd035225a3c9838b8264a730d086
Mike Crockett <github@mmcrockett.com>
2017-02-08 12:32:18 -0600

    Making key depend on rails env.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_EloHockey_session'
+Rails.application.config.session_store :cookie_store, :key => "#{Rails.env}_elo_hockey_session"
