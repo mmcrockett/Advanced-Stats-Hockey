56b56fcd9bdd6945c55529a3d8c676254ccde6ac
Mike Crockett <github@mmcrockett.com>
2017-05-22 11:21:00 -0500

    Upgrading versions, adding indicator on chart load.

--- app/assets/javascripts/angular/controllers/elos.controller.js
+++ app/assets/javascripts/angular/controllers/elos.controller.js
@@ -10,20 +10,26 @@ function(
   JsLiteral
 ) {
   $scope.error = "";
+  $scope.progress = {message:""}
   $scope.eloData = [];
   $scope.eloChart = {
     "type": "LineChart"
   };
+  $scope.clear_progress = function() {
+    $scope.progress.message = "";
+  };
   $scope.initialize = function() {
+    $scope.progress.message = "Loading data...";
     Elo
     .query()
     .$promise
     .then(
       function(data){
+        $scope.progress.message = "Rendering chart...";
         $scope.jsLiteralData = JsLiteral.from_json(data);
 
         $scope.eloChart.data = $scope.jsLiteralData;
-        Logger.debug("Retrieved elos, count '" + $scope.eloData.length + "'.");
+        Logger.debug("Retrieved elos, count '" + data.length + "'.");
       }
     ).catch(
       function(e){
